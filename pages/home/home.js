c9d2ffaac165542a6afba0e024f2d63917f86506
小黄风云 <1005038958@qq.com>
2020-04-01 23:01:22 +0800

    知识点3

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,30 @@
 // pages/home/home.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    name:"World",
+    age:18,
+    students:[
+      {id:110, name:'kobe', age:28},
+      {id:111,name:'laosi',age:18},
+      {id:112,name:'zhangsan',age:38},
+      {id:113,name:'lili',age:32}
+    ],
+    counter:0
   },
+  handleBtnClick(){
+    // 错误做法，数据是不会刷新的
+    // this.data.counter += 1
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    this.setData({
+      counter:this.data.counter+1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction(){
+    this.setData({
+      counter:this.data.counter-1
+    })
   }
 })
