074e2a7daef855fddaa149ddc50af86f76cad959
小黄风云 <1005038958@qq.com>
2020-04-09 13:22:48 +0800

    组件化开发

---
+++ components/my-mslot/my-mslot.js
@@ -0,0 +1,29 @@
+// components/my-mslot/my-mslot.js
+Component({
+  // 定义组件的配置选项
+  // multipleSlots: 在使用多插槽时需要设置true
+  // styleIsolation: 设置样式的隔离方式
+  options: {
+    multipleSlots: true
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
